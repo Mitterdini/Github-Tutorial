69e9484a87c56c9a1a126ceb7e851be224c6cd11
Mitterdini <31043944+Mitterdini@users.noreply.github.com>
2018-05-25 16:37:40 -0400

    I made dictionary files

--- Scripts/case_switch.py
+++ Scripts/case_switch.py
@@ -1,8 +1,9 @@
 print("What up nigga")
 x=input("what's your favorite color: ")
-def switch_demo(argument):
+def switch_demo(choice):
 	switcher = {
-		"green":"dang, I dig",
-		"blue":"not what i'd pick but ight"}
-	print(switcher.get(argument, "Invalid color"))
+		"green":"dang, I dig",					 #Dictionaries use {Key:Value}
+		"blue":"not what I'd pick but ight"}	 
+	print(switcher.get(choice, "Invalid color")) #.get has a built in error slot
+												 #example.get(arg, "error MSG")
 switch_demo(x)
---
+++ Scripts/dictionaries.py
@@ -0,0 +1,24 @@
+clap = 5
+dat = "Ass"
+
+dict = {"succ":"is short for succle", clap:dat, 7: 45}                          #You ca set variables, strings, or numbers
+x = input("pick something: ")
+print(dict.get(x),"\n")                                                         #This only works with strings => conversions don't count.
+
+print("""\nwhen using a variable you need to call the variable
+not what it's assigned to:""", dict.get(clap))                                  #This works when the variable is predefined
+print("\n")
+
+dict["Tom"] = "Is a female dog"                                                 #Another way to add keys/balues to a dictionary
+dict[3] = "Musketeers"
+dict["two"] = 2                                                                 #REMEMBER keys are case-sensitive
+
+print(dict, "\n")                                                               #Printing values
+print(dict.get("Tom"))                                                          #User input may not be able to call variable/numbered keys
+print(dict.get(3))                                                              #But you can still call them using >example.get('key')<
+print(dict.get("two"))
+
+#You can send an error message if a key is not in a dictionary
+print('\n', dict.get(5,"ERROR: Not even close."))                                      #Watch out because assigned variable values hold the same
+print(dict.get(8, "ERROR: numbers are neat right!!"))
+print(dict.get("goof", "ERROR AGAIN: welp, still wrong"))
